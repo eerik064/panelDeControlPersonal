6949762f43472456d14cdf8e20b5e4faee008521
eerik064 <erik.espindola@uab.edu.bo>
2026-08-04 20:56:21 -0400

    Inplementacion de validacion de formulario a nivel js

--- script.js
+++ script.js
@@ -25,3 +25,36 @@ function alternarMenu() {
 }
 
 botonMenu.addEventListener("click", alternarMenu);
+
+const formularioEnvio = document.querySelector("#form-envio");
+const aviso = document.querySelector("#aviso");
+
+function revisarEnvio(event) {
+
+    event.preventDefault();
+
+    const nombre = document.querySelector("#nombre").value;
+    const email = document.querySelector("#email").value;
+    const mensaje = document.querySelector("#mensaje").value;
+
+    if (nombre === "") {
+        aviso.textContent = "Porfavor ingrese su nombre.";
+        aviso.classList.add("error");
+        aviso.classList.remove("exito");
+    } else if (email.includes("@") === false) {
+
+        aviso.textContent = "Ingrese un correo valido";
+        aviso.classList.add("error");
+        aviso.classList.remove("exito");
+    } else if (mensaje === "") {
+        aviso.textContent = "Porfavor llene el campo mensaje";
+        aviso.classList.add("error");
+        aviso.classList.remove("exito");
+    } else {
+        aviso.textContent = "Mensaje enviado exitosamente.";
+        aviso.classList.add("exito");
+        aviso.classList.remove("error");
+    }
+}
+
+formularioEnvio.addEventListener("submit", revisarEnvio);
